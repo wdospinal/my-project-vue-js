457ac9083a6bf05be40ea94e52fbcc817662b274
Wilson Daniel Ospina <dospinal@psl.com.co>
2017-03-22 19:44:09 -0500

    Initial vue.js files

---
+++ src/containers/CreateSnipmark.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Hola Medellin.js </h1>
+</template>
+
+<script>
+  export default {
+    name: 'CreateSnipmarks',
+  };
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Hello from '@/components/Hello';
+import home from '@/containers/Home';
+import CreateSnipmarks from '@/containers/CreateSnipmark';
 
 Vue.use(Router);
 
@@ -8,8 +9,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello,
+      name: 'Home',
+      component: home,
+    },
+    {
+      path: '/create',
+      name: 'CreateSnipmarks',
+      component: CreateSnipmarks,
     },
   ],
 });
